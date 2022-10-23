13b745c63d199f82fab71ee27c4e999aa7e8ac7c
Brandon Extra <brandex@Brandons-MacBook-Air.local>
2022-10-23 12:19:35 -0400

    stylesheet layout

--- recipe-application/src/components/Recipe/recipe.js
+++ recipe-application/src/components/Recipe/recipe.js
@@ -1,6 +1,7 @@
 // import { urlencoded } from "body-parser";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "./recipe.css";
 
 const Recipe = () => {
   const { recipeId } = useParams();
