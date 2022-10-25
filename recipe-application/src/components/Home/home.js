c4823b77047cce00a6fe0704ca8b258341e7ba22
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-10-25 19:53:38 -0400

    home styling + changed title colors also for about and contact

--- recipe-application/src/components/Home/home.js
+++ recipe-application/src/components/Home/home.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./home.css";
+import meal from "../../../src/images/homePic.jpeg";
 
 const Home = () => {
   return (
     <div>
       <div id="title">
         <h1>Welcome to our Recipe Application!</h1>
+      </div>
+      <div id="img-holder">
+        <img id="meal" alt="ingredients" src={meal} />
+        <p>
+          This application is meant to make life a little easier for you in the
+          kitchen; head to the recipes tab to go crazy with your meals!
+        </p>
       </div>
     </div>
   );
